6c1f81dba1e5d08e1144df49db06228ee56c79ea
Albert Byrone <albertbyrone1677@gmail.com>
2023-03-09 18:20:28 +0300

    remove for recreation

--- api/db/migrate/20230309061906_create_tech_stacks.rb
+++
@@ -1,9 +0,0 @@
-class CreateTechStacks < ActiveRecord::Migration[7.0]
-  def change
-    create_table :tech_stacks do |t|
-      t.string :name
-      t.string :description
-      t.string :logo
-    end
-  end
-end
--- api/db/migrate/20230309140052_fix_password_hash.rb
+++
@@ -1,5 +0,0 @@
-class FixPasswordHash < ActiveRecord::Migration[7.0]
-  def change
-    rename_column :users, :password_hash, :password_digest
-  end
-end
