ad6172bc0e96ed31b3ddd43cb57742e11f5d448e
Albert Byrone <albertbyrone1677@gmail.com>
2023-03-09 10:22:33 +0300

    update the user table

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_secure_password
+  
   has_many :projects
   belongs_to :primary_tech_stack, class_name: 'TechStack', foreign_key: 'primary_tech_stack', optional: true
   enum role: [:admin, :reviewer, :user]
