b45c8c046f8863425f1ff75b73853a7aa1b1765d
Ian Okumu <okumu.otsembo@gmail.com>
2023-03-10 23:08:27 +0300

    Test: Fix API failure due to active record

--- api/app/models/project_comment.rb
+++ api/app/models/project_comment.rb
@@ -1,4 +1,4 @@
-class ProjectComment < ApplicationRecord
+class ProjectComment < ActiveRecord::Base
   belongs_to :user
   belongs_to :project
 
--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -1,4 +1,4 @@
-class User < ApplicationRecord
+class User < ActiveRecord::Base
   has_secure_password
 
   has_many :projects
