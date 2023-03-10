67ee362483db2b4cbc44cbc0c32b057e51b5c58f
Titus Muthomi <muthomititus+github@gmail.com>
2023-03-10 11:41:14 +0300

    Handle user registration and generate a token using jwt

--- api/app/controllers/user_controller.rb
+++ api/app/controllers/user_controller.rb
@@ -40,7 +40,27 @@ class UserController < Sinatra::Base
     json_response(code: 404, data: { error: 'You seem lost. That page does not exist!' })
   end
 
-  
+  # @API: Register a new user
+  post '/register' do
+    data = JSON.parse(request.body.read)
+
+    email = data['email']
+    password = data['password']
+
+    if email.nil? || password.nil?
+      error_response(err: 'Email and password are required')
+    end
+
+    # generate jwt token
+    payload = { email: email }
+    token = JWT.encode payload, JWT_SECRET, 'HS256'
+
+    # save user to db
+    user = User.new(email: email, password: password, token: token)
+    user.save
+
+    json_response(code: 201, data: { token: token })
+  end
   
 
 end
