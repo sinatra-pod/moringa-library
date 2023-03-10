68f4f860cc191f44f69a93de853d490131bfb9fd
Titus Muthomi <muthomititus+github@gmail.com>
2023-03-10 11:40:40 +0300

    Handle bad request and not found error as well as json format response

--- api/app/controllers/user_controller.rb
+++ api/app/controllers/user_controller.rb
@@ -2,7 +2,45 @@ require 'sinatra'
 require 'jwt'
 
 class UserController < Sinatra::Base
+  JWT_SECRET = 'my$ecretK3y'
 
+  # @API: Enable CORS headers from all origins
+  configure do
+    enable :cross_origin
+  end
+
+  before do
+    response.headers['Access-Control-Allow-Origin'] = '*'
+  end
+
+  options '*' do
+
+    response.headers['Allow'] = 'GET, PUT, POST, DELETE, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Authorization, Content-Type, Accept, X-User-Email, X-Auth-Token'
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    200
+  end
+
+  # @API: Format all JSON responses
+  def json_response(code: 200, data: nil)
+    status = [200, 201].include?(code) ? 'SUCCESS' : 'FAILED'
+    headers['Content-Type'] = 'application/json'
+    return unless data
+
+    [code, { data: data, message: status }.to_json]
+  end
+
+  # @API: Format all common error responses as JSON (thrown errors)
+  def error_response(err:, code: 422)
+    json_response(code: code, data: { error: err.message })
+  end
+
+  # @API: 404 handler
+  not_found do
+    json_response(code: 404, data: { error: 'You seem lost. That page does not exist!' })
+  end
+
+  
   
 
 end
