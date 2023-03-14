921535d3c3aa7e2a25ac5d6bbdcf56ec3aaa8a1f
Calebbii <biicaleb316@gmail.com>
2023-03-14 15:39:40 +0300

    commit adds the category section

--- webapp/src/pages/AllSubmissionView.tsx
+++ webapp/src/pages/AllSubmissionView.tsx
@@ -21,6 +21,21 @@ function AllSubmissionView() {
                     </div>
                 </form>
 
+
+                <h3><em>FILTER BY</em></h3>
+
+                {/* Category Section */}
+                <h3>Categories</h3>
+                
+                <div className="flex items-center mb-4">
+                    <input id="default-radio-1" type="radio" value="" name="default-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
+                    <label htmlFor="default-radio-1" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Default radio</label>
+                </div>
+                <div className="flex items-center">
+                    <input checked id="default-radio-2" type="radio" value="" name="default-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
+                    <label htmlFor="default-radio-2" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Checked state</label>
+                </div>
+
                 
 
                 
