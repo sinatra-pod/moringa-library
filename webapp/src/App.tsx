c3984c9f54ff3ab686da0953d6f4fa66190964d4
Machoka Daisy <machokadaisy25@gmail.com>
2023-03-16 19:55:38 +0300

    add route for signup page

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import HomeView from "./pages/HomeView";
 import LoginView from "./pages/LoginView";
 import RegisterView from "./pages/RegisterView";
+import SignupView from "./pages/SignupView";
 
 function App() {
   return (
@@ -9,6 +10,9 @@ function App() {
       <Route path="/" element={<HomeView />} />
       <Route path="/login" element={<LoginView />} />
       <Route path="/register" element={<RegisterView />} />
+      <Route path="/signup" element={<SignupView />} />
+
+
     </Routes>
   );
 }
