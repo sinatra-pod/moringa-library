e00dca97f0d0cbacd00f3d46eb8e277900bbfde1
Titus Muthomi <muthomititus+github@gmail.com>
2023-03-20 12:50:56 +0300

    generate token only after save

--- api/app/controllers/user_controller.rb
+++ api/app/controllers/user_controller.rb
@@ -13,7 +13,7 @@ class UserController < Base
 
     # generate jwt token
     payload = { email: email }
-    token = JWT.encode payload, JWT_SECRET, 'HS256'
+   
 
     # save user to db
     user = User.new(email: email, password: password, token: token)
@@ -23,6 +23,8 @@ class UserController < Base
     if user.errors.any?
       error_response(err: user.errors.full_messages.join(', '))
     end
+
+    token = JWT.encode payload, JWT_SECRET, 'HS256'
 
     json_response(code: 201, data: { token: token })
   end
