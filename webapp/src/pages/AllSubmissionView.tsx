90973af8ab2f2812e554acd5f2eb5c48635ce765
Calebbii <biicaleb316@gmail.com>
2023-03-21 10:05:56 +0300

    adds the main content section

--- webapp/src/pages/AllSubmissionView.tsx
+++ webapp/src/pages/AllSubmissionView.tsx
@@ -9,17 +9,6 @@ function AllSubmissionView() {
             <div className='bg-white w-1/5'>
                    {/* Sidebar content here */}
 
-                   {/* Search section  */}
-                <form>   
-                    <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-                    <div className="relative">
-                        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                            <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
-                        </div>
-                        <input type="search" id="default-search" className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required/>
-                        <button type="submit" className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
-                    </div>
-                </form>
 
 
                 <h3><em>FILTER BY</em></h3>
@@ -109,9 +98,38 @@ function AllSubmissionView() {
                 <header className='bg-white border-b border-gray-200'>
                 {/* <!-- Header content here --> */}
 
+
+                {/* <input type="text" placeholder="Search..." /> */}
+                   {/* Search section  */}
+                   <form>   
+                    <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+                    <div className="relative">
+                        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                            <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+                        </div>
+                        <input type="search" id="default-search" className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required/>
+                        <button type="submit" className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+                    </div>
+                </form>
+
+
                 </header>
                 <main className='p-4'>
                 {/* <!-- Main content here --> */}
+                <div className="grid grid-cols-3 gap-4">
+                    <div className="p-4">
+                        <img src="https://img.freepik.com/free-photo/medium-shot-man-wearing-vr-glasses_23-2149126949.jpg?size=626&ext=jpg" alt="vrproject" />
+                        The Force
+                        <p>The force is here. A web application for Star Wars fans; get information about all the films in the franchise.</p>
+                        <div>
+                            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-0.5 px-2 rounded">Submit</button>
+                        </div>
+                    </div>
+                    <div className="p-4">Item 2</div>
+                    <div className="p-4">Item 3</div>
+                    <div className="p-4">Item 4</div>
+                    <div className="p-4">Item 5</div>
+                </div>
                 </main>
             </div>
         </div>
