43e6a9d0a86cce673ed1d4bdc7a15af49218d70d
Titus Muthomi <muthomititus+github@gmail.com>
2023-03-22 14:10:52 +0300

    Add additional user fields

--- api/app/controllers/user_controller.rb
+++ api/app/controllers/user_controller.rb
@@ -1,22 +1,19 @@
-
-
 class UserController < Base
-
-
   # @API: Register a new user
   post '/register' do
     data = JSON.parse(request.body.read)
 
     email = data['email']
     password = data['password']
-
+    role = data['role']
+    gh_username = data['gh_username']
 
     # generate jwt token
     payload = { email: email }
    
 
     # save user to db
-    user = User.new(email: email, password: password, token: token)
+    user = User.new(email: email, password: password, role:role, gh_username:gh_username, token: token)
     user.save
 
     # check if user save is successful
@@ -28,7 +25,4 @@ class UserController < Base
 
     json_response(code: 201, data: { token: token })
   end
-  
-
-
 end
